416bfd1ed5da253978e71026416bc90371d826be
Unviray <unviray@gmail.com>
2020-07-27 15:57:18 +0300

    Adding tests

---
+++ tests/test_tag.py
@@ -0,0 +1,53 @@
+from beam import *
+
+
+def test_main():
+    component = div(
+        h1('Title'),
+        _class="content",
+    )
+
+    assert component.render() == '<div class="content"><h1>Title</h1></div>'
+
+
+def test_formating():
+    component = h1('My name is {name}')
+
+    component['name'] = 'Alice'
+    assert component.render() == '<h1>My name is Alice</h1>'
+
+    component['name'] = 'John Doe'
+    assert component.render() == '<h1>My name is John Doe</h1>'
+
+
+def test_recursive_formating():
+    component = div('I like these color',
+        ul(
+            li('{spec}'),
+            li('Green'),
+            li('Blue'),
+        )
+    )
+
+    component['spec'] = 'Red'
+    assert component.render() == (
+        '<div>'
+            'I like these color'
+            '<ul>'
+                '<li>Red</li>'
+                '<li>Green</li>'
+                '<li>Blue</li>'
+            '</ul>'
+        '</div>'
+    )
+    component['spec'] = 'Purple'
+    assert component.render() == (
+        '<div>'
+            'I like these color'
+            '<ul>'
+                '<li>Purple</li>'
+                '<li>Green</li>'
+                '<li>Blue</li>'
+            '</ul>'
+        '</div>'
+    )
