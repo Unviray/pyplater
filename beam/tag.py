5c8aac979c1950b2a96196b13510c2729813ccc9
Unviray <unviray@gmail.com>
2020-07-29 11:00:58 +0300

    refactor code

--- beam/tag.py
+++ beam/tag.py
@@ -1,4 +1,13 @@
+"""
+beam.tag.
+
+Include all tags.
+"""
+
+
 class Element(object):
+    """Base element of any tags."""
+
     TAG_RAW = "<{tag_name} {params}>{contents}</{tag_name}>"
     # TAG_RAW = "<Element class='bg-dark'>The element</Element>"
 
@@ -15,7 +24,7 @@ class Element(object):
             # ex: class="bg-{color}"
             result = f'{key.replace("_", "")}="{self.param[key]}"'
             # ex: class="bg-dark"
-            params.append(self.formating(result))                
+            params.append(self.formating(result))
 
         for content in self.content:
             if isinstance(content, Element):
@@ -26,14 +35,14 @@ class Element(object):
             contents.append(self.formating(result))
 
         if len(params) == 0:
-            tag_raw = self.TAG_RAW.replace(' ', '')
+            tag_raw = self.TAG_RAW.replace(" ", "")
         else:
             tag_raw = self.TAG_RAW
 
         return tag_raw.format(
             tag_name=self.__class__.__name__,
-            params=' '.join(params),
-            contents=''.join(contents),
+            params=" ".join(params),
+            contents="".join(contents),
         )
 
     def formating(self, text):
@@ -75,11 +84,14 @@ class SingleElement(Element):
 
 # Base
 
+
 class html(Element):
     pass
 
+
 class head(Element):
     pass
+
 
 class body(Element):
     pass
@@ -87,17 +99,22 @@ class body(Element):
 
 # Head
 
+
 class link(SingleElement):
     pass
+
 
 class meta(SingleElement):
     pass
 
+
 class script(Element):
     pass
 
+
 class style(Element):
     pass
+
 
 class title(Element):
     pass
@@ -105,101 +122,134 @@ class title(Element):
 
 # Structure
 
+
 class abbr(Element):
     pass
+
 
 class blockquote(Element):
     pass
 
+
 class cite(Element):
     pass
+
 
 class q(Element):
     pass
 
+
 class sup(Element):
     pass
+
 
 class sub(Element):
     pass
 
+
 class strong(Element):
     pass
+
 
 class em(Element):
     pass
 
+
 class mark(Element):
     pass
+
 
 class h1(Element):
     pass
 
+
 class h2(Element):
     pass
+
 
 class h3(Element):
     pass
 
+
 class h4(Element):
     pass
+
 
 class h5(Element):
     pass
 
+
 class h6(Element):
     pass
+
 
 class img(SingleElement):
     pass
 
+
 class figure(Element):
     pass
+
 
 class figcaption(Element):
     pass
 
+
 class audio(Element):
     pass
+
 
 class video(Element):
     pass
 
+
 class source(Element):
     pass
+
 
 class a(Element):
     pass
 
+
 class br(SingleElement):
     pass
+
 
 class p(Element):
     pass
 
+
 class hr(SingleElement):
     pass
+
 
 class address(Element):
     pass
 
+
 class _del(Element):
     pass
+
 
 class ins(Element):
     pass
 
+
 class dfn(Element):
     pass
+
 
 class kbd(Element):
     pass
 
+
 class pre(Element):
     pass
 
+
 class progress(Element):
     pass
+
 
 class time(Element):
     pass
@@ -207,20 +257,26 @@ class time(Element):
 
 # List
 
+
 class ul(Element):
     pass
+
 
 class ol(Element):
     pass
 
+
 class li(Element):
     pass
+
 
 class dl(Element):
     pass
 
+
 class dt(Element):
     pass
+
 
 class dd(Element):
     pass
@@ -228,26 +284,34 @@ class dd(Element):
 
 # Table
 
+
 class table(Element):
     pass
+
 
 class caption(Element):
     pass
 
+
 class tr(Element):
     pass
+
 
 class th(Element):
     pass
 
+
 class td(Element):
     pass
+
 
 class thead(Element):
     pass
 
+
 class tbody(Element):
     pass
+
 
 class tfoot(Element):
     pass
@@ -255,32 +319,42 @@ class tfoot(Element):
 
 # Form
 
+
 class form(Element):
     pass
+
 
 class fieldset(Element):
     pass
 
+
 class legend(Element):
     pass
+
 
 class label(Element):
     pass
 
+
 class _input(SingleElement):
     pass
+
 
 class button(Element):
     pass
 
+
 class textarea(Element):
     pass
+
 
 class select(Element):
     pass
 
+
 class option(Element):
     pass
+
 
 class optgroup(Element):
     pass
@@ -288,20 +362,26 @@ class optgroup(Element):
 
 # Section
 
+
 class header(Element):
     pass
+
 
 class nav(Element):
     pass
 
+
 class footer(Element):
     pass
+
 
 class section(Element):
     pass
 
+
 class article(Element):
     pass
+
 
 class aside(Element):
     pass
@@ -309,8 +389,10 @@ class aside(Element):
 
 # Generic
 
+
 class div(Element):
     pass
+
 
 class span(Element):
     pass
--- tests/test_tag.py
+++ tests/test_tag.py
@@ -1,53 +1,45 @@
-from beam import *
+from beam import div, h1, ul, li
 
 
 def test_main():
-    component = div(
-        h1('Title'),
-        _class="content",
-    )
+    component = div(h1("Title"), _class="content",)
 
     assert component.render() == '<div class="content"><h1>Title</h1></div>'
 
 
 def test_formating():
-    component = h1('My name is {name}')
+    component = h1("My name is {name}")
 
-    component['name'] = 'Alice'
-    assert component.render() == '<h1>My name is Alice</h1>'
+    component["name"] = "Alice"
+    assert component.render() == "<h1>My name is Alice</h1>"
 
-    component['name'] = 'John Doe'
-    assert component.render() == '<h1>My name is John Doe</h1>'
+    component["name"] = "John Doe"
+    assert component.render() == "<h1>My name is John Doe</h1>"
 
 
 def test_recursive_formating():
-    component = div('I like these color',
-        ul(
-            li('{spec}'),
-            li('Green'),
-            li('Blue'),
-        )
-    )
+    component = div("I like these color",
+                    ul(li("{spec}"), li("Green"), li("Blue"),))
 
-    component['spec'] = 'Red'
+    component["spec"] = "Red"
     assert component.render() == (
-        '<div>'
-            'I like these color'
-            '<ul>'
-                '<li>Red</li>'
-                '<li>Green</li>'
-                '<li>Blue</li>'
-            '</ul>'
-        '</div>'
+        "<div>"
+        "I like these color"
+        "<ul>"
+        "<li>Red</li>"
+        "<li>Green</li>"
+        "<li>Blue</li>"
+        "</ul>"
+        "</div>"
     )
-    component['spec'] = 'Purple'
+    component["spec"] = "Purple"
     assert component.render() == (
-        '<div>'
-            'I like these color'
-            '<ul>'
-                '<li>Purple</li>'
-                '<li>Green</li>'
-                '<li>Blue</li>'
-            '</ul>'
-        '</div>'
+        "<div>"
+        "I like these color"
+        "<ul>"
+        "<li>Purple</li>"
+        "<li>Green</li>"
+        "<li>Blue</li>"
+        "</ul>"
+        "</div>"
     )
