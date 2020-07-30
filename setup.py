d5c34828c2d9fd1f247bfc0ecc5bc9b294ba382a
Unviray <unviray@gmail.com>
2020-07-30 13:32:14 +0300

    Adding required file

--- setup.py
+++ setup.py
@@ -1,22 +1,21 @@
-from setuptools import setup, find_packages
-
+from setuptools import find_packages, setup
 
 setup(
     author="Unviray",
     author_email="unviray@gmail.com",
     python_requires=">=3.7",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-            'Programming Language :: Python :: 3.7',
-    ]
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+    ],
     description="Build html component with python",
     license="MIT license",
     keywords="html component react angular vue javascript js nodejs node",
     name="beam",
-    packages=find_packages(include=['beam', 'beam.*']),
-    url='https://github.com/Unviray/beam',
-    version='0.1.0',
+    packages=find_packages(include=["beam", "beam.*"]),
+    url="https://github.com/Unviray/beam",
+    version="0.1.0",
     zip_safe=False,
 )
